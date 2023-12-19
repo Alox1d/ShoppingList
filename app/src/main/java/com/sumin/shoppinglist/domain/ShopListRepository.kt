754da6ec85f52a6b92c3a1b0f2795fa2c5b693cc
Alox1d <alox1dab@gmail.com>
2023-12-19 21:15:22 +0300

    added domain layer

---
+++ app/src/main/java/com/sumin/shoppinglist/domain/ShopListRepository.kt
@@ -0,0 +1,10 @@
+package com.sumin.shoppinglist.domain
+
+interface ShopListRepository {
+    fun addShopItem(shopItem: ShopItem)
+    fun editShopItem(shopItem: ShopItem)
+    fun deleteShopItem(shopItem: ShopItem)
+    fun getShopItem(id: Int): ShopItem
+    fun getShopList(): List<ShopItem>
+
+}
